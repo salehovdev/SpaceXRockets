ff914029331ca7e65d004bee57ea6097dbfc8e2d
Fuad Salehov <salehoviosdev@gmail.com>
2025-07-15 20:16:29 +0200

    [Create] Authentication Group

---
+++ SpaceXRockets/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 15.07.25.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
---
+++ SpaceXRockets/Authentication/AuthenticationViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationViewModel.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 15.07.25.
+//
+
+import Foundation
+
+@MainActor
+class AuthenticationViewModel: ObservableObject {
+    
+}
