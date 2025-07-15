81c8b6420ef6fc174e8558a28e97d45345ed366c
Fuad Salehov <salehoviosdev@gmail.com>
2025-07-15 19:24:22 +0200

    Initial Commit

---
+++ SpaceXRockets/SpaceXRocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXRocketsApp.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 15.07.25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXRocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
