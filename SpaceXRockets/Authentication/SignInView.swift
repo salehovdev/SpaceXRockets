16d6f9513e49d6e6b69f8a719415c09ca90b1a0a
Fuad Salehov <salehoviosdev@gmail.com>
2025-07-15 23:12:55 +0200

    [Create] Adding SignInView

---
+++ SpaceXRockets/Authentication/SignInView.swift
@@ -0,0 +1,47 @@
+//
+//  SignInView.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 15.07.25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @StateObject var viewModel = AuthenticationViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Email...", text: $viewModel.email)
+                    .padding()
+                    .background(.gray.opacity(0.4))
+                    .clipShape(.rect(cornerRadius: 10))
+                
+                SecureField("Password...", text: $viewModel.password)
+                    .padding()
+                    .background(.gray.opacity(0.4))
+                    .clipShape(.rect(cornerRadius: 10))
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign in")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
