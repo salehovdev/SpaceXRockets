eaeae8235c4c052e414ab80804be10955a8599ab
Fuad Salehov <salehoviosdev@gmail.com>
2025-07-19 13:40:07 +0200

    [Create] Create Rockets model

---
+++ SpaceXRockets/RocketsModel.swift
@@ -0,0 +1,33 @@
+//
+//  RocketsModel.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 19.07.25.
+//
+
+import Foundation
+
+struct Rockets: Codable {
+    let name: String
+    let firstFlight: String
+    let successRatePct: Int
+    let mass: Mass
+    let height: Diameter
+    let diameter: Diameter
+    
+    enum CodingKeys: String, CodingKey {
+        case name, height, mass, diameter
+        case firstFlight = "first_flight"
+        case successRatePct = "success_rate_pct"
+    }
+}
+
+struct Diameter: Codable {
+    let meters: Double?
+    let feet: Double?
+}
+
+struct Mass: Codable {
+    let kg: Int
+    let lb: Int
+}
