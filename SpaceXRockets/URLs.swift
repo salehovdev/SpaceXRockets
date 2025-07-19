ba4d9b48e323d94d5f5e9c9c74ba5144499bf081
Fuad Salehov <salehoviosdev@gmail.com>
2025-07-19 14:24:12 +0200

    [Create] Url struct

---
+++ SpaceXRockets/URLs.swift
@@ -0,0 +1,13 @@
+//
+//  URLs.swift
+//  SpaceXRockets
+//
+//  Created by Fuad Salehov on 19.07.25.
+//
+
+import Foundation
+
+struct URLs {
+    static let rocketUrl = "https://api.spacexdata.com/v4/rockets"
+    static let launchUrl = "https://api.spacexdata.com/v5/launches"
+}
